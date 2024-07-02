e78c41a5656f748e6d30f25236ccb9b9dbef86bf
Ajeesh <ajeeshrs79@gmail.com>
2024-07-02 16:14:49 +0530

    Chat creating logic updated

--- src/controllers/chatController.ts
+++ src/controllers/chatController.ts
@@ -2,19 +2,43 @@ import { Request, Response } from "express";
 import { Chat } from "../models/chatModel";
 
 export const creatNewChat = async (message: any) => {
-    try {
-        const { userName, peerUserName, id, peerId } = message
-        console.log(userName, peerUserName, id, peerId)
+  try {
+    const { userName, peerUserName, id, peerId } = message;
+    console.log(userName, peerUserName, id, peerId);
+    const chatName = `${userName} and ${peerUserName}`;
+    const possibleChatName = `${peerUserName} and ${userName}`;
 
-        const chatName = `${userName} and ${peerUserName}`
-        await Chat.create({
-            chatName: chatName,
-            members: [id, peerId]
-        })
+    const isChatExist = await Chat.findOne({
+      $or: [{ chatName: chatName }, { chatName: possibleChatName }],
+    });
 
-        console.log(`${chatName} chat created`)
-
-    } catch (err) {
-        console.error("Error in creating new chat", err)
+    if (isChatExist) {
+      console.log("chat already exist for these users: ", isChatExist);
+      return;
     }
-}
+
+    await Chat.create({
+      chatName: chatName,
+      members: [id, peerId],
+    });
+
+    console.log(`${chatName} chat created`);
+  } catch (err) {
+    console.error("Error in creating new chat", err);
+  }
+};
+
+export const createNewMessage = async (
+  sender: string,
+  chatName: string,
+  message: string,
+  recipient: string
+) => {
+  try {
+    if (!sender || !chatName || !message || !recipient) {
+      console.log("Missing details");
+    }
+  } catch (err) {
+    console.error("Error in creating new message: ", err);
+  }
+};
